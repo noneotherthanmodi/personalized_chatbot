9f7a002ef682ac0e350db75de81060da99f9a250
noneotherthanmodi <uditmodi03@gmail.com>
2024-02-22 13:44:44 +0530

    personalized chatbot

---
+++ pdf_chatbot.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import openai 
+import langchain
+import streamlit as st 
+
+from config import OPENAI_API_KEY, HUGGINGFACE_API_KEY
+
+openai.api_key = OPENAI_API_KEY
+
+def main():
+    st.set_page_config(page_title="Chat with multiple pdfs",page_icon=":books:")
+    st.header("Chat with multiple pdfs :books:")
+    st.text_input("Ask any question about your pdf: ")
+
+    with st.sidebar:
+        st.subheader("Your documents: ")
+        st.file_uploader("Upload your file here and click on 'Process'.")
+        st.button("Process")
+
+
+if __name__ == "__main__":
+    main()
